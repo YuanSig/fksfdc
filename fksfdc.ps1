3d7e1c5b5629f38e51f9c76f8343d0c846861a7d
Kota Baba <kbaba@vmware.com>
2019-01-06 01:17:08 +0900

    add This is my first commit!

---
+++ fksfdc.ps1
@@ -0,0 +1,61 @@
+﻿$N_weeks_ago = 1
+calc_man_hour(extract_calender_items($N_weeks_ago))
+
+
+#get your calender items as a hash 
+Function extract_calender_items($N_weeks_ago){
+    $olFolderCalendar = 9
+    $ol = New-Object -ComObject Outlook.Application
+    $ns = $ol.GetNamespace('MAPI')
+    $Monday = get_lastMonday($N_weeks_ago)
+    $Friday = $Monday.AddDays(7).ToShortDateString()
+    $Monday = $Monday.ToShortDateString()
+    $Filter = "[MessageClass]='IPM.Appointment' AND [Start] > '$Monday' AND [End] < '$Friday'"
+    $WeeklyCalendarItems = New-Object System.Collections.ArrayList
+    $ns.GetDefaultFolder($olFolderCalendar).Items.Restrict($Filter) | foreach{
+        If($_.RecurrenceState -eq 0){
+        $WeeklyCalendarItems += $_
+        }
+    }
+    Return $WeeklyCalendarItems
+}
+
+
+Function get_lastMonday ($N_weeks_ago){
+
+    switch ((Get-Date).DayOfWeek){
+        "Monday"{$LastMonday = (Get-Date).AddDays(0-$N_weeks_ago*7)}
+        "Tuesday"{$LastMonday = (Get-Date).AddDays(-1-$N_weeks_ago*7)}
+        "Wednesday"{$LastMonday = (Get-Date).AddDays(-2-$N_weeks_ago*7)}
+        "Thursday"{$LastMonday = (Get-Date).AddDays(-3-$N_weeks_ago*7)}
+        "Friday"{$LastMonday = (Get-Date).AddDays(-4-$N_weeks_ago*7)}
+        "Saturday"{$LastMonday = (Get-Date).AddDays(-5-$N_weeks_ago*7)}
+        "Sunday"{$LastMonday = (Get-Date).AddDays(-6-$N_weeks_ago*7)}
+        default {Write-Host "What day is it $_ ??"}
+
+    }
+    Return $LastMonday
+}
+
+
+#arrange and consolidate "WeeklyCalendarItems"
+#get a hash {category, sum of durations}
+Function calc_man_hour($WeeklyCalendarItems){
+    $i = 0
+    $hours_per_category_hash = @{}
+    $WeeklyCalendarItems.Categories | foreach{
+        If($_ -ne ""){
+            #add the duration to the exsisting key value
+            If($hours_per_category_hash.ContainsKey($_)){
+                $hours_per_category_hash[$_] = $hours_per_category_hash[$_] + $WeeklyCalendarItems.Duration[$i]
+            }
+            #add the key
+            else{
+                $hours_per_category_hash.Add($_ ,$WeeklyCalendarItems.Duration[$i])
+            }
+        }
+    $i++
+    }
+    Return $hours_per_category_hash
+}
+
